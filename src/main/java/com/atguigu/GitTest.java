5d90f1d43ca89de74bc8ce76d3c4ac86f0f856d1
yq <2483245070@qq.com>
2022-06-02 10:04:25 +0800

    创建文件

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+/**
+ * @author yq
+ * @create 2022-06-06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
