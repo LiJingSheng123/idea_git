a65e412e49de72333797c4af3599f2aa2367419a
yq <2483245070@qq.com>
2022-06-02 10:06:30 +0800

    第二次修改

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
